b4f712ee115ceda76e2c155caab4fa8bf55afee5
benjaminharuna1 <benjaminharuna099@gmail.com>
2022-06-27 06:45:15 +1200

    updated task
    updated task now working

--- benjaminharuna1/blog/views.py
+++ benjaminharuna1/blog/views.py
@@ -1,29 +1,38 @@
+from django.shortcuts import render
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
 from .models import Post
-
-class PostLisyView:
-    model = Post
+# Create your views here.
 
 
-class PostCreateView:
-    model = Post
-
-class PostCreateView:
-    model = Post
-    fields = "__all__"
-    success_url = reverse_lazy("blog:all")
-
-class PostDetailView:
-    model = Post
-
-class PostUpdateView:
-    model = Post
-    fields = "__all__"
-    success_url = reverse_lazy("blog:all")
-
-class PostDeleteView:
-    model = Post
-    fields = "__all__"
+class PostListView(ListView):
+  model = Post
+  def get_queryset(self):
+    return super().get_queryset().filter(status="published")
 
 
+class PostCreateView(CreateView):
+  model = Post
+  fields = "__all__"
+  success_url  = reverse_lazy("blog:all")
+
+
+
+class PostDetailView(DetailView):
+  model = Post
+
+  
+
+class  PostUpdateView(UpdateView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+  model = Post
+  fields = "__all__"
+  success_url = reverse_lazy("blog:all")
